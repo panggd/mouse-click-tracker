d16c237d77b373d80fc56f3bcc6e7d568b372a3a
Pang Gin Dee <pang.gindee@itdea.biz>
2020-03-08 16:26:30 +0800

    Add send message to producer logic

--- api-kafka/services/publish-producer.js
+++ api-kafka/services/publish-producer.js
@@ -1,10 +1,32 @@
-const publishProducer = async (req, res) => {
+const moment = require('moment');
+const publishProducer = async (req, res, producer) => {
+  let results = {};
   try {
-
+    const topic = process.env.KAFKA_TOPIC;
+    payload = JSON.stringify(req.body);
+    console.log(payload);
+    let payloads = [
+      {
+        topic: topic,
+        messages: payload,
+        partition: 0,
+        timestamp: moment.unix()
+      }
+    ];
+    producer.send(payloads, (err, data) => {
+      if (err) throw new Error(err);
+      console.log(data);
+      res.status(204);
+    });
   } catch(error) {
     console.error(error, error.stack);
+    results = {
+      "error": `Failed to publish to ${topic}, ${payload}`
+    };
+    res.status(500);
   }
-  res.send("hello from producer");
+  res.append('Content-Type', 'application/json');
+  res.send(results);
 };
 
 module.exports = publishProducer;
