f461d89cde311e1dc985e8e43aa9040cb7ecb9d3
Pang Gin Dee <pang.gindee@itdea.biz>
2020-03-10 15:16:09 +0800

    Refactor to get websocket working

--- api-kafka/index.js
+++ api-kafka/index.js
@@ -14,8 +14,7 @@ const KAFKA_HOST = process.env.KAFKA_HOST;
 const KAFKA_PORT = process.env.KAFKA_PORT;
 const KAFKA_TOPIC = process.env.KAFKA_TOPIC;
 
-const client = new kafka.KafkaClient({ kafkaHost: `${KAFKA_HOST}:${KAFKA_PORT}` });
-const producer = new kafka.Producer(client, { requireAcks: 1 });
+var producer = null;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -23,26 +22,38 @@ app.use(bodyParser.json());
 app.post("/publish", (req, res) => publishToProducer(req, res, producer));
 app.ws("/stream", (ws, req) => {});
 
+// TODO: how to wait for kafka container
+// to complete before starting the api container
 setTimeout(() => {
+
   app.listen(PORT, () => {
-    producer.on("ready", () => {
-      console.log("kafka producer is ready");
+    const client = new kafka.KafkaClient({
+      kafkaHost: `${KAFKA_HOST}:${KAFKA_PORT}`
     });
-    producer.on('error', (err) => {
-      console.error(err, err.stack);
-    });
-    const wss = expressWS.getWss('/stream');
     const consumer = new kafka.Consumer(client, [{
       topic: KAFKA_TOPIC,
       offset: 0,
       partition: 0
     }]);
+
+    producer = new kafka.Producer(client, { requireAcks: 1 });
+
+    producer.on("ready", () => {
+      console.log("kafka producer is ready");
+    });
+
+    producer.on('error', (err) => {
+      console.error(err, err.stack);
+    });
+
+    const wss = expressWS.getWss('/stream');
+
     consumer.on('message', (message) => {
-      console.log(["test", message]);
       wss.clients.forEach((client) => {
-        client.send(message);
+        client.send(JSON.stringify(message));
       });
     });
+
     console.log(`Kafka api server listening on port ${PORT}!`);
   });
 }, WAIT_MS);
